68f40b2934cdf4a5bd6acc71c01fb3df2315a026
mbinary <zhuheqin1@gmail.com>
2018-03-24 20:44:52 +0800

    finish

---
+++ hello_linux.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#########################################################################
+# File Name: hello_linux.sh
+# Author: mbinary
+# mail: zhuheqin1@gmail.com
+# Created Time: Sat 24 Mar 2018 08:38:19 PM DST
+#########################################################################
+echo "Hello Linux"
+echo  -n >  output.txt  # cat /dev/null > output.txt
+while read -r  line
+do
+	echo $line >> output.txt
+done
+
+#a simpler solution
+#cat   > output.txt
